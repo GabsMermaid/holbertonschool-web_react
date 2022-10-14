56a958293973d0b71eec68890ba8af3d5ccb88fe
Gabriela Martinez <g.martinez.rodz@gmail.com>
2022-10-13 22:44:28 -0400

    Task 4

---
+++ 0x00-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+namespace Subjects {
+    export class Subject  {
+      teacher: Teacher;
+  
+      set setTeacher(teacher: Teacher) {
+        this.teacher = teacher;
+      }
+    }
+  }
+  Footer
+  
